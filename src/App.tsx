6adb26c9cb615c3bd579a971f9cbf0e9140b9651
moshedr <dmd6708713@gmai.com>
2024-04-30 23:57:07 +0300

    The beginning of a manager

--- src/App.tsx
+++ src/App.tsx
@@ -4,58 +4,56 @@ import BuildingStructure from './cllasas/buildingStructure';
 import Elevator from './cllasas/elevator';
 
 
-function App(): JSX.Element {
-  const buildingStructure = new BuildingStructure(8, 3);
-  const floors: JSX.Element[] = [];  
-  const elevtars: JSX.Element[] = []; 
-  const elevator = new Elevator(0, 4, "e", 1);
-  const handleFloorButtonClick = (floorNumber: number) => {
-    // קריאה לפונקציה שמבצעת את התנועה לקומה המתאימה באובייקט המעלית
-    elevator.moveToFloor(floorNumber);
-    console.log(elevator.currentFloor);
-    const elevatorImg = document.querySelector('.elevator-img') as HTMLElement;
-    if (elevatorImg) {
-        elevatorImg.style.transform = `translateY(-${floorNumber * 110 }px)`;
-        
-        console.log(elevator.currentFloor)
-    }
-};
-
-
-
-  for (let i = 0; i < buildingStructure.numElevators; i++) {        
-    elevtars.unshift(      
-      <div key={i} >
-        <img className='elevator-img' src={'/elv.png'} alt="מעלית" />
-        {/* <p>{elevator.currentFloor}</p> */}
-      </div>
-    );
-}
-
-  // פה אני יוצר את הקומות בהתאם לבקשה 
-  for (let i = 0; i <= buildingStructure.numFloors; i++) {
-    const floor = new Floor(i);    
-    floors.unshift(      
-      <div key={i} className="floor">  
-      <div className='container-button'>     
-      <button className="metal linear" onClick={() => handleFloorButtonClick(floor.floorNumber)}> {floor.floorNumber} </button>
-           
-        </div> 
-      </div>
-    );
-}
-
-
+function App(){
+  // const buildingStructure = new BuildingStructure(8, 3);
+  
   return (
-    <div className='container'>
-    <div className='container-floor'>
-      {floors} {/* תצוגת כל הקומות */}
-    </div>
-    <div className='container-ele'>
-      {elevtars}
-      </div>
+    <div className='container'>  
+    
+    <BuildingStructure numFloors={6} numElevators={1}/>
+     
     </div>
   );
 }
 
 export default App;
+
+
+
+//   const handleFloorButtonClick = (floorNumber: number) => {
+//     // קריאה לפונקציה שמבצעת את התנועה לקומה המתאימה באובייקט המעלית
+//     elevator.moveToFloor(floorNumber);
+//     console.log(elevator.currentFloor);
+//     const elevatorImg = document.querySelector('.elevator-img') as HTMLElement;
+//     if (elevatorImg) {
+//         elevatorImg.style.transform = `translateY(-${floorNumber * 110 }px)`;
+        
+//         console.log(elevator.currentFloor)
+//     }
+// };
+
+
+
+//   for (let i = 0; i < buildingStructure.numElevators; i++) {        
+//     elevtars.unshift(      
+//       <div key={i} >
+//         <img className='elevator-img' src={'/elv.png'} alt="מעלית" />        
+//       </div>
+//     );
+// }
+
+  
+//   for (let i = 0; i <= buildingStructure.numFloors; i++) {
+//     const floor = new Floor(i);    
+//     floors.unshift(      
+//       <div key={i} className="floor">  
+//       <div className='container-button'>     
+//       <button className="metal linear" onClick={() => handleFloorButtonClick(floor.floorNumber)}> {floor.floorNumber} </button>
+           
+//         </div> 
+//       </div>
+//     );
+// }
+
+
+  
--- src/cllasas/buildingStructure.tsx
+++ src/cllasas/buildingStructure.tsx
@@ -1,39 +1,76 @@
-import Elevator  from './elevator';
-import Floor  from './floor';
+import React, { Component } from 'react';
+import Elevator from './elevator';
+import Floor from './floor';
 
-class BuildingStructure {    
+// סוג ה-props של BuildingStructure
+interface BuildingStructureProps {
+    numFloors: number;
+    numElevators: number;
+}
+
+// מחלקת המבנה של הבניין
+class BuildingStructure extends Component<BuildingStructureProps> {
+    // הגדרת התכונות של המבנה
     numFloors: number;
     numElevators: number;
     elevators: Elevator[];
     floors: Floor[];
-    
-    constructor(numFloors: number, numElevators: number) {
-        this.numFloors = numFloors;
-        this.numElevators = numElevators;
+
+    // הבנאי של המחלקה
+    constructor(props: BuildingStructureProps) {
+        super(props);
+        this.numFloors = props.numFloors;
+        this.numElevators = props.numElevators;
         this.elevators = [];
         this.floors = [];
-    }
-
-
-    addElevator(elevator: Elevator): void {
-        // להוספת מעלית למערכת
-    }
-
-    removeElevator(elevator: Elevator): void {
-        // להסרת מעלית מהמערכת
-    }
-
-    addFloor(floor: Floor): void {
-        // להוספת קומה למערכת
-    }
-
-    removeFloor(floor: Floor): void {
-        // להסרת קומה מהמערכת
-    }
+        for (let i = 0; i <= this.numFloors; i++) {
+            const floor = new Floor({floorNumber: i});
+            this.floors.unshift(floor);
+        }
+        for (let i = 0; i < this.numElevators; i++) {
+            const elevator = new Elevator({
+                currentFloor: 0,
+                destinationFloor: 0,
+                status: 'stopped',
+                estimatedTimeToDestination: 0
+            });
+            this.elevators.unshift(elevator)
+        }
+    }  
 
     findNearestElevator(floorNumber: number): Elevator {
         // לחיפוש למעלית הקרובה ביותר
         return this.elevators[0]; // זמני
     }
+    Calling_the_elevator(): void {
+
+    }
+
+    render(): JSX.Element {
+        return (
+            <div className="building-structure">
+                <div className="container-ele">
+                    <div className='elevator-img'>
+                        {this.elevators.map((elevator, index) => (
+                        <Elevator key={index} />
+                        ))}
+                    </div>
+                </div>
+
+                <div className="container-floor">
+                    {this.floors.map((floor, index) => (
+                        <div key={index} className='floor'>                            
+                        
+                        <div className='container-button'>
+                            <button className="metal linear">{index}</button>
+                            </div>    
+                            <Floor floorNumber={index} />
+                        </div>                        
+                    ))}
+                </div>            
+            </div>
+        );
+    }
 }
+
 export default BuildingStructure;
