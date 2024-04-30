fa44da23cbac80693ffecee3a7034136f279e28f
moshedr <dmd6708713@gmai.com>
2024-04-30 10:48:07 +0300

    final_on_floor

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import Floor from './cllasas/floor';
 import BuildingStructure from './cllasas/buildingStructure';
 
+
 function App(): JSX.Element {
-  const buildingStructure = new BuildingStructure(3, 2);
-  const floors: JSX.Element[] = [];
+  const buildingStructure = new BuildingStructure(8, 2);
+  const floors: JSX.Element[] = [];  
 
   // פה אני יוצר את הקומות בהתאם לבקשה 
-  for (let i = 1; i <= buildingStructure.numFloors; i++) {
-    const floor = new Floor(i);
-    floors.push(
+  for (let i = 0; i <= buildingStructure.numFloors; i++) {
+    const floor = new Floor(i);    
+    floors.unshift(      
       <div key={i} className="floor">
         <p> {floor.floorNumber}</p>
+        
       </div>
     );
   }
